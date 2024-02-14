43a3173da778a95bd45a9004f0dc27343bfec87a
Antonio Peña <angeles_yanco@hotmail.com>
2024-02-13 19:53:41 -0600

    48. Routing in Laravel

---
+++ resources/views/tienda.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <a href="/">Principal</a>
+        <a href="/tienda">Tienda</a>
+        <a href="/nosotros">Nosotros</a>
+    </nav>
+    <h1>Tienda</h1>
+</body>
+</html>
