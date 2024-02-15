832fe15124380a6481f657cb0d73f0b3e28e7a94
Antonio Peña <angeles_yanco@hotmail.com>
2024-02-14 19:07:19 -0600

    50. Installing Tailwind CSS with Vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,22 +6,14 @@
 
         <title>Devstagram - @yield('titulo')</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
+        @vite('resources/css/app.css')
     </head>
     <body>
-        <nav>
-            <a href="/">Principal</a>
-            <a href="/tienda">Tienda</a>
-            <a href="/nosotros">Nosotros</a>
-        </nav>
-        <h1>@yield('titulo')</h1>
 
-        <hr>
+    <h1 class="text-4xl">@yield('titulo')</h1>
 
-        @yield('contenido')
+    <hr>
 
+    @yield('contenido')
     </body>
 </html>
