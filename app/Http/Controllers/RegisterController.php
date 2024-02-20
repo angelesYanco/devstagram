3799ce0180be1d2e640fa1668579efe18940fa55
Antonio Peña <angeles_yanco@hotmail.com>
2024-02-19 19:25:07 -0600

    73. Hashear Passwords

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -29,6 +30,8 @@ class RegisterController extends Controller
             'username' => $request->username,
             'email' => $request->email,
             'password' => $request->password
+            // 'password' => Hash::make($request->password) [VERSION 9]
+            
         ]);
     }
 }
