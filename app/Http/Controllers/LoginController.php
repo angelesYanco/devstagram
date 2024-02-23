b35eed4a044d55b0504f050c5c04f952a86298cd
Antonio Peña Angeles <angeles_yanco@hotmail.com>
2024-02-22 22:09:31 -0600

    85. Keep the session always open.

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -11,12 +11,13 @@ class LoginController extends Controller
         return view('auth.login');
     }
     public function store(Request $request){
+
         $this->validate($request,[
             'email' => 'required|email',
             'password' => 'required'
         ]);
 
-        if(!auth()->attempt($request->only('email', 'password'))){
+        if(!auth()->attempt($request->only('email', 'password'), $request->remember)){
             return back()->with('mensaje','Credenciales Incorrectas');
         }
 
