0164c519769dcee3b2a2605ed3039e9fb0d41d45
Antonio Peña Angeles <angeles_yanco@hotmail.com>
2024-02-23 19:50:28 -0600

    91. Finalizing the Form.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -14,21 +14,41 @@
             <form action="{{route('register')}}" method="POST" novalidate>
                 @csrf
                 <div class="mb-5">
-                    <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
-                        Nombre
+                    <label for="titulo" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Título
                     </label>
                     <input 
-                        id="name"
-                        name="name" 
+                        id="titulo"
+                        name="titulo" 
                         type="text" 
-                        placeholder="Tu Nombre"
+                        placeholder="Título de la publicación"
                         class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror"
                         value="{{ old('name') }}"                        
                     >
-                    @error('name')
+                    @error('titulo')
                         <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
                     @enderror
                 </div>
+
+                <div class="mb-5">
+                    <label for="descripcion" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Descripción
+                    </label>
+                    <textarea 
+                        id="descripcion"
+                        name="descripcion" 
+                        placeholder="Descripción de la Publicación"
+                        class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror"
+                    >{{ old('descripcion') }}</textarea>
+                    @error('titulo')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                    @enderror
+                </div>
+                <input 
+                    type="submit"
+                    value="Publicar"
+                    class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"
+                >
             </form>
         </div>
 
